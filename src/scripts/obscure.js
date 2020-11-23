e35d5032312605d1b0b2f2c23e5aa4f2e01a3641
Jordin Brown <jbrown@cognetif.com>
2020-11-23 08:21:10 -0500

    change to dynamic properties and less variable declarations.

--- src/scripts/obscure.js
+++ src/scripts/obscure.js
@@ -1,45 +1,22 @@
-(function() {
+(function () {
 
-  this.Obscure = function() {
+    this.Obscure = function () {
 
-    let obscure = document.querySelectorAll('[obscure]');
+        let newNode;
+        document.querySelectorAll('[obscure]').forEach(elem => {
 
-    for (let i = 0; i < obscure.length; i++) {
-      // Get Email
-      let local = obscure[i].getAttribute('data-local')
-      let domain = obscure[i].getAttribute('data-domain')
-      
-      // Get Phone Number
-      let country = obscure[i].getAttribute('data-country')
-      let area = obscure[i].getAttribute('data-area')
-      let prefix = obscure[i].getAttribute('data-prefix')
-      let line = obscure[i].getAttribute('data-line')
-      
-      // Check if it's a phone number
-      if (obscure[i].hasAttribute('data-country')) {
-        
-        // If it is, let's put out a phone number
-        let tel = document.createElement('a');
-        tel.className = 'tel';
-        tel.href = `tel:+${country}${area}${prefix}${line}`;
-        tel.appendChild(document.createTextNode(`(${area}) ${prefix}-${line}`));
-        
-        obscure[i].parentNode.insertBefore(tel, obscure[i]);
-      
-      } else {
-        
-        // Otherwise, we'll default to emails
-        let email = document.createElement('a'); 
-        email.className = 'mail';
-        email.href = `mailto:${local}@${domain}`;
-        email.appendChild(document.createTextNode(`${local}@${domain}`))
-        
-        obscure[i].parentNode.insertBefore(email, obscure[i]);
-        
-      }
+            newNode = document.createElement('span');
+            newNode.innerHTML = elem.innerHTML;
+            elem.classList ? newNode.classList = elem.classList : false;
+            elem.id ? newNode.id = elem.id : false;
 
+            for (let prop in elem.dataset) {
+                newNode.innerHTML = newNode.innerHTML.replaceAll('%' + prop.replace('p', ''), elem.dataset[prop]);
+            }
+
+            elem.parentNode.insertBefore(newNode, elem);
+            elem.remove();
+        });
     }
 
-  }
-
-} ());
+}());
