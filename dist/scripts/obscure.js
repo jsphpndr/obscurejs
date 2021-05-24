b788aa7e8391d1f76891920c2895a8887e417d5b
Joseph Pinder <hello@josephpinder.com>
2021-05-24 14:14:28 -0400

    v2

--- dist/scripts/obscure.js
+++ dist/scripts/obscure.js
@@ -1,32 +1,18 @@
 (function () {
   this.Obscure = function () {
-    var obscure = document.querySelectorAll('[obscure]');
+    var newNode;
+    document.querySelectorAll('[obscure]').forEach(function (elem) {
+      newNode = document.createElement('span');
+      newNode.innerHTML = elem.innerHTML;
+      elem.classList ? newNode.classList = elem.classList : false;
+      elem.id ? newNode.id = elem.id : false;
 
-    for (var i = 0; i < obscure.length; i++) {
-      // Get Email
-      var local = obscure[i].getAttribute('data-local');
-      var domain = obscure[i].getAttribute('data-domain'); // Get Phone Number
-
-      var country = obscure[i].getAttribute('data-country');
-      var area = obscure[i].getAttribute('data-area');
-      var prefix = obscure[i].getAttribute('data-prefix');
-      var line = obscure[i].getAttribute('data-line'); // Check if it's a phone number
-
-      if (obscure[i].hasAttribute('data-country')) {
-        // If it is, let's put out a phone number
-        var tel = document.createElement('a');
-        tel.className = 'tel';
-        tel.href = "tel:+".concat(country).concat(area).concat(prefix).concat(line);
-        tel.appendChild(document.createTextNode("(".concat(area, ") ").concat(prefix, "-").concat(line)));
-        obscure[i].parentNode.insertBefore(tel, obscure[i]);
-      } else {
-        // Otherwise, we'll default to emails
-        var email = document.createElement('a');
-        email.className = 'mail';
-        email.href = "mailto:".concat(local, "@").concat(domain);
-        email.appendChild(document.createTextNode("".concat(local, "@").concat(domain)));
-        obscure[i].parentNode.insertBefore(email, obscure[i]);
+      for (var prop in elem.dataset) {
+        newNode.innerHTML = newNode.innerHTML.replaceAll('%' + prop.replace('p', ''), elem.dataset[prop]);
       }
-    }
+
+      elem.parentNode.insertBefore(newNode, elem);
+      elem.remove();
+    });
   };
 })();
